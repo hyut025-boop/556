008d352ba585b9f5b028ab13159fc0143bbfbf84
hyut025-boop <harayuto0205@gmail.com>
2026-05-24 16:17:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,15 @@
-const CACHE = 'alarm-pwa-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
+const CACHE = 'gogoro-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
