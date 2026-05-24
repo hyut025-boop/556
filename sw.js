696d65cc581c9933ae2fc4259cb47365643d5070
hyut025-boop <harayuto0205@gmail.com>
2026-05-24 16:37:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE = 'gogoro-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE = 'gogoro-v3';
+const ASSETS = ['./', './index.html', './manifest.json', './logo.png', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    Promise.all(keys.map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
